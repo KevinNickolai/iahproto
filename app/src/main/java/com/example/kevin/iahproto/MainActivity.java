6e5188c384d0baf7d53db9b7fd349ca9399695b0
KevinNickolai <nickola6@msu.edu>
2017-11-09 13:36:27 -0500

    Fixed gradle build issues
    26.0.2 SDK -> 27.0.0

--- app/src/main/java/com/example/kevin/iahproto/MainActivity.java
+++ app/src/main/java/com/example/kevin/iahproto/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.ComponentName;
+import android.widget.ExpandableListView;
 
 
 import java.util.List;
@@ -14,19 +15,23 @@ public class MainActivity extends AppCompatActivity {
 
     PackageManager m_pm;
     List<ApplicationInfo> m_blockedApps;
+    ExpandableListView m_appsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //get the pm
         m_pm = getPackageManager();
         List<ApplicationInfo> allApps = m_pm.getInstalledApplications(0);
+
+        m_appsList = findViewById(R.id.expandListViewAppsList);
 
         for(int i = 0; i < allApps.size();++i)
         {
             //display application name in selection list of apps to block
-
+            
         }
     }
 
@@ -43,9 +48,8 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 0; i < m_blockedApps.size();++i)
         {
             try{
-                //the name of the component we're blocking
-                ComponentName cn = new ComponentName(m_blockedApps.get(i).packageName,m_blockedApps.get(i).className);
-                m_pm.setComponentEnabledSetting(cn,
+                //freeze all blocked applications.
+                m_pm.setApplicationEnabledSetting(m_blockedApps.get(i).packageName,
                         PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                         PackageManager.DONT_KILL_APP);
             }
@@ -60,8 +64,7 @@ public class MainActivity extends AppCompatActivity {
     {
         for(int i = 0; i < m_blockedApps.size(); ++i)
         {
-            ComponentName cn = new ComponentName((m_blockedApps.get(i).packageName,m_blockedApps.get(i).className));
-            m_pm.setComponentEnabledSetting(cn,
+            m_pm.setApplicationEnabledSetting(m_blockedApps.get(i).packageName,
                     PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
                     PackageManager.DONT_KILL_APP);
         }
