4bec2ec24675ff201a665d015c4ff4e2eb2c177e
KevinNickolai <nickola6@msu.edu>
2017-11-28 17:37:58 -0500

    removed List Adapter overrides and now instead using ArrayAdapter
    ArrayAdapter allows for ListViews to display items from a given List, in this case the list of installed applications.

--- app/src/main/java/com/example/kevin/iahproto/AppListActivity.java
+++ app/src/main/java/com/example/kevin/iahproto/AppListActivity.java
@@ -3,16 +3,10 @@ package com.example.kevin.iahproto;
 import android.app.ListActivity;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.database.DataSetObserver;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ExpandableListView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-
 import java.util.List;
 
 /**
@@ -37,73 +31,8 @@ public class AppListActivity extends ListActivity {
         //get our ListView
         m_appsList = findViewById(R.id.listViewAppList);
 
-        
-        m_appsList.setAdapter(new ListAdapter() {
-            @Override
-            public boolean areAllItemsEnabled() {
-                return false;
-            }
-
-            @Override
-            public boolean isEnabled(int i) {
-                return false;
-            }
-
-            @Override
-            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-
-            }
-
-            @Override
-            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-            }
-
-            @Override
-            public int getCount() {
-                return 0;
-            }
-
-            @Override
-            public Object getItem(int i) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int i) {
-                return 0;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return false;
-            }
-
-            @Override
-            public View getView(int i, View view, ViewGroup viewGroup) {
-                return null;
-            }
-
-            @Override
-            public int getItemViewType(int i) {
-                return 0;
-            }
-
-            @Override
-            public int getViewTypeCount() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-        });
-        for(int i = 0; i < allApps.size();++i)
-        {
-            //display application name in selection list of apps to block
-
-        }
+        ArrayAdapter<ApplicationInfo> adapter = new ArrayAdapter<ApplicationInfo>(this, android.R.layout.simple_list_item_1,allApps);
+        m_appsList.setAdapter(adapter);
     }
 
     public void onOkPressed(View view)
